bb0b828e4f7a0ba342392759c0d97809cb40ccda
victorcotap <levass.v@gmail.com>
2018-11-26 22:58:51 -0800

    Populating zone framework

---
+++ Conquest/ConquestSpawn.lua
@@ -0,0 +1,40 @@
+env.info("CON: Preparing troops")
+-- "Blue Capture Zone Spawn #2(g)"
+-- "Blue Zone Assault #1(b)"
+-- "Blue Zone Escort #1(a)"
+
+CaptureZoneBaseString = "Capture Zone Spawn #"
+CaptureZoneBaseTemplateCount = 4
+CaptureZoneBaseTemplateGroupCount = 7
+
+
+-- BLUECaptureZoneBaseString = "Blue Capture Zone Spawn #"
+-- REDCaptureZoneBaseString = "Red Capture Zone Spawn #"
+-- REDCaptureZoneBaseTemplateCount = 4
+
+-- BLUEZoneAssaultString = "Blue Zone Assault #1"
+-- BLUEZONEEscortString = "Blue Zone Escort #1"
+-- REDZoneAssaultString = "Red Zone Assault #1"
+-- REDZONEEscortString = "Red Zone Escort #1"
+
+local SpawnsTable = {
+    ["Blue"] = {},
+    ["Red"] = {}
+}
+
+function SpawnZoneBaseSpawn(coalitionString, zone)
+    local random = math.random( 1, CaptureZoneBaseTemplateCount )
+    local baseString = coalitionString .. " " .. CaptureZoneBaseString .. tostring(random)
+    for i = 1, BLUECaptureZoneBaseTemplateCount do
+        local spawnString = baseString .. "(" .. tostring(i) .. ")"
+        local spawn = SpawnsTable[coalitionString][spawnString]
+        if spawn == nil then
+            local newSpawn = SPAWN:New(spawnString)
+            SpawnsTable[coalitionString][spawnString] = newSpawn
+            spawn = newSpawn
+        end
+        spawn:SpawnInZone(zone, true)
+    end
+end
+
+env.info("CON: Troops are ready")
--- Conquest/ConquestZones.lua
+++ Conquest/ConquestZones.lua
@@ -1,5 +1,47 @@
 env.info('CON: Zones loading')
+function tableLength(T)
+    local count = 0
+    for _ in pairs(T) do count = count + 1 end
+    return count
+end
+function randomizeSelectTable(maxIndex, maxCount)
+    local SelectedTable = {}
+    for order, zone in pairs(SelectedTable) do
+        local zoneSelectRandom = math.random( 1, ZoneToPopulateCount )
+        SelectedTable[zoneSelectRandom] = true
+        if tableLength(SelectedTable) == maxCount then
+            break
+        end
+    end
+    return SelectedTable
+end
 
 
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+CaptureZoneString = "Capture Zone #"
+REDCaptureZoneString = "Red Capture Zone #"
+
+local ZoneCount = 15
+local ZoneToPopulateCount = 5
+
+
+function populateZones(coalitionString, selectedZones)
+    for index, value in pair(selectedZones) do
+        local zoneString = coalitionString .. " " .. CaptureZoneString .. tostring(index)
+        local zone = ZONE:New(zoneString)
+        SpawnZoneBaseRandomSpawn(coalitionString, zone)
+    end
+end
+
+function startPopulateZones(something)
+    local SelectedBlueZonesTable = randomizeSelectTable(ZoneCount, ZoneToPopulateCount)
+    local SelectedRedZonesTable = randomizeSelectTable(ZoneCount, ZoneToPopulateCount)
+    env.info("CON: Selected Blue Zones " .. UTILS.OneLineSerialize(SelectedBlueZonesTable))
+
+    populateZones("Blue", SelectedBlueZonesTable)
+    populateZones("Red", SelectedRedZonesTable)
+end
+SCHEDULER:New(nil, populateZones, {"sdfsdfd"}, 5)
 
 env.info('CON: Zones finished')
